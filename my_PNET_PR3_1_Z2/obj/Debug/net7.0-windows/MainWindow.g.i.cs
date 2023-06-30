c40420950bc984b34741573c4eb3d1a8fb3f3235
AnjoYT <AnjoYT@wp.pl>
2023-06-30 13:28:47 +0200

    IMPORT/EXPORT

--- my_PNET_PR3_1_Z2/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ my_PNET_PR3_1_Z2/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace my_PNET_PR3_1_Z2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/my_PNET_PR3_1_Z2;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/my_PNET_PR3_1_Z2;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- my_PNET_PR3_1_Z3/MainWindow.xaml.cs
+++ my_PNET_PR3_1_Z3/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +47,82 @@ namespace my_PNET_PR3_1_Z3
         private void Delete(object sender, RoutedEventArgs e)
         {
             Movies.Remove((Modify_Movies)list.SelectedItem);
+        }
+        private void Import(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text Files (*.txt)|*.txt"; 
+            if (openFile.ShowDialog() == true)
+            {
+                string filePath = openFile.FileName;
+                ImportTxt(filePath);
+            }
+        }
+        private void ImportTxt(string filePath) 
+        {
+            string[] strings = File.ReadAllLines(filePath);
+            try
+            {
+                foreach (string line in strings)
+                {
+                    string[] component = line.Split(';');
+                    if (component.Length == 5)
+                    {
+                        Modify_Movies movie = new Modify_Movies
+                        {
+
+                            Title = component[0],
+                            Director = component[1],
+                            Producer = component[2],
+                            DataCT = component[3]
+                        };
+                    
+                        DateTime releaseDate;
+                        if (DateTime.TryParse(component[4], out releaseDate))
+                        {
+                            movie.ReleaseDate = releaseDate;
+                        }
+
+                        Movies.Add(movie);
+
+                    }
+                    else {
+                        MessageBox.Show("ERROR: please implement full information, for example: DAVE;John Krasinsky;John Krasinsky;10/05/2000");
+                    }
+                }
+            }
+            catch(Exception e) 
+            {
+                MessageBox.Show("ERROR: Change format, for example: DAVE;John Krasinsky;John Krasinsky;10/05/2000");
+            }
+        
+        }
+        private void Export(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Text Files (*.txt)|*.txt";
+            if (saveFile.ShowDialog() == true)
+            {
+                string filePath = saveFile.FileName;
+                ExportTxt(filePath);
+            }
+        }
+        private void ExportTxt(string filePath)
+        {
+            try
+            {
+                List<string> strings = new List<string>();
+                foreach (Modify_Movies movie in Movies)
+                {
+                    string line = $"{movie.Title};{movie.Director};{movie.Producer};{movie.DataCT};{movie.ReleaseDate?.ToString() ?? string.Empty}";
+                    strings.Add(line);
+                }
+                File.WriteAllLines(filePath, strings);
+            }
+            catch(Exception e ) 
+            {
+                MessageBox.Show($"Error occurred while exporting data: {0}", e.Message);
+            }
         }
     }
 }
